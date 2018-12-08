1c2ea00677a45b02f59895b85347323eb29d00ca
Jake Derry <jacob.derry@duke.edu>
2018-12-07 21:26:08 -0500

    Update HuffProcessor.java

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -80,11 +80,11 @@ public class HuffProcessor {
 	 */
 	private void writeHeader(HuffNode root, BitOutputStream out) {
 		if (root.myLeft == null && root.myRight == null) {
-			out.write(1);
+			out.writeBits(1, 1);
 			out.writeBits(BITS_PER_WORD + 1, root.myValue);
 		}
 		else {
-			out.write(0);
+			out.writeBits(1, 0);
 			writeHeader(root.myLeft, out);
 			writeHeader(root.myRight, out);
 		}
